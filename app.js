852b2b8284c057ff6d00aa7e1d6ca6bfe9e7fc27
ZayLong <zaylong10@gmail.com>
2021-01-19 04:09:43 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,42 @@
+//Selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+//Event Listners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event){
+    event.preventDefault(); // can stop default behavior from button click i.e. refreshing page on submit
+    
+    // div wrapper for element 
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    
+    // creates li element for a list item
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // TRASH/DELETE BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+
+    //clear input
+    todoInput.value = '';
+
+}
